8085f9b969be9e19578025af388e58105a013588
David Purdy <dave.purdy1@gmail.com>
2020-10-06 00:45:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(string) {
   console.log (string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  return string('I can't hear you! .toLowerCase())
-  return string('Y')
+  return string.toLowerCase()
+  return string
 }
