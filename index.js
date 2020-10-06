c41d019b0b806a2d96dde29a3087e35e11e4c052
David Purdy <dave.purdy1@gmail.com>
2020-10-06 00:01:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function logShout(string) {
-  console.log string.toUpperCase()
+console.log logShout(string) {
+  return string.toUpperCase()
 }
